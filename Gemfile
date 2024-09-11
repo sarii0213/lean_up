f1fdbd54455f9f5a5cd206a4e3af6017184a9429
sarii0213 <anjing.singka@gmail.com>
2024-09-11 14:10:14 +0900

    :heavy_plus_sign: devise install & config
    ユーザ一覧ページをroot pageにしてログイン等のテストに使う

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'seed-fu'
 
 gem 'annotate'
 
+gem 'devise'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows], require: 'debug/prelude'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,19 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string(191)      default(""), not null
-#  username   :string(191)      default(""), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  email                  :string(191)      default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string(191)      default(""), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 FactoryBot.define do
   factory :user do
