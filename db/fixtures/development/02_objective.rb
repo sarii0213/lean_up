6c0c7bf628aaa9d6d2b8ed08f1fd074b40df56ba
sarii0213 <anjing.singka@gmail.com>
2024-09-14 21:29:38 +0900

    :+1: objectivesに画像タイプのseedも追加

--- db/fixtures/development/02_objective.rb
+++ db/fixtures/development/02_objective.rb
@@ -1,5 +1,7 @@
 Objective.seed(:id,
-               { id: 1, objective_type: 1, verbal: '買い物を楽しむ!! "入る服"ではなく"好きな服"!!', comment: '', user_id: 1, order: 0 },
-               { id: 2, objective_type: 1, verbal: '邪魔な脂肪を落として綺麗な筋肉の輪郭を掘り起こす', comment: '', user_id: 1, order: 1 }
+               { id: 1, objective_type: 1, verbal: '服の買い物がぐっと楽しくなる', comment: '', user_id: 1, order: 0 },
+               { id: 2, objective_type: 0, verbal: '', comment: '秋になったらゲットする', user_id: 1, order: 1 },
+               { id: 3, objective_type: 1, verbal: '脂肪の下に隠れた美しい筋肉', comment: '', user_id: 1, order: 2 }
 )
-# todo: objective_type: 0 でimageを登録できるようにしてそのseedを足す
+Objective.find(2).images.attach( io: File.open(Rails.root.join('db/images/lilith_black.png')), filename: 'lilith_black.png' )
+
