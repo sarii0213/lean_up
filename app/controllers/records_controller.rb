d4318c53b1a931f51ef123b65a011acfb67bf88c
sarii0213 <anjing.singka@gmail.com>
2024-10-08 18:54:02 +0900

    :+1: メッセージ機能をcontrollerからmodelに移動

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -15,7 +15,8 @@ class RecordsController < ApplicationController
     @record = current_user.records.find_or_initialize_by(recorded_on: record_params[:recorded_on])
     @record.assign_attributes(record_params)
     if @record.save
-      redirect_to records_path, notice: "記録完了! #{generate_message(@record.recorded_on)}"
+      message = Record::MessageGenerator.new(@record.recorded_on).generate
+      redirect_to records_path, notice: "記録完了! #{message}"
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,20 +26,5 @@ class RecordsController < ApplicationController
 
   def record_params
     params.require(:record).permit(:recorded_on, :weight, :body_fat)
-  end
-
-  def generate_message(recorded_on)
-    # TODO: 生理周期登録機能実装後、メッセージ生成時に生理周期も参考にするように改善
-    trend = Record.moving_average_trend(recorded_on)
-    messages = { plateau: plateau_messages, smooth: smooth_messages }
-    trend ? messages[trend].sample : 'データが入るとメッセージが表示されます'
-  end
-
-  def plateau_messages
-    %w[現状把握できてて偉い!! 体重とかまじでただの数字 大きな増加や停滞はだいたい水分]
-  end
-
-  def smooth_messages
-    %w[減っている!!すごい!! いい調子で減量中!!]
   end
 end
