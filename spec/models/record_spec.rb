024f16efe4c29ef5abea852cbd21a6da64dd176f
sarii0213 <anjing.singka@gmail.com>
2024-10-08 18:54:59 +0900

    :white_check_mark: Record#moving_average_trendでnilを返すケース追加

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -29,30 +29,38 @@ RSpec.describe Record, type: :model do
     let(:recorded_on) { Date.new(2024, 1, 20) }
     let(:user) { create(:user) }
 
-    # 01/01: 60.0, 01/07: 61.0, 01/13: 62.0
-    before do
-      [1, 7, 13].each_with_index do |n, i|
-        create(:record, user:, recorded_on: Date.new(2024, 1, n), weight: 60.0 + i)
+    context '体重記録が２つ以上ある場合' do
+      # 01/01: 60.0, 01/07: 61.0, 01/13: 62.0
+      before do
+        [1, 7, 13].each_with_index do |n, i|
+          create(:record, user:, recorded_on: Date.new(2024, 1, n), weight: 60.0 + i)
+        end
+      end
+
+      context '記録日の平均値 >= 一つ前の記録日の平均値 の場合' do
+        before do
+          create(:record, user:, recorded_on:, weight: 63.0)
+        end
+
+        it ':plateau（=停滞中）を返す' do
+          expect(trend).to eq :plateau
+        end
+      end
+
+      context '記録日の平均値 < 一つ前の記録日の平均値 の場合' do
+        before do
+          create(:record, user:, recorded_on:, weight: 59.0)
+        end
+
+        it ':smooth（＝順調）を返す' do
+          expect(trend).to eq :smooth
+        end
       end
     end
 
-    context '記録日の平均値 >= 一つ前の記録日の平均値 の場合' do
-      before do
-        create(:record, user:, recorded_on:, weight: 63.0)
-      end
-
-      it ':plateau（=停滞中）を返す' do
-        expect(trend).to eq :plateau
-      end
-    end
-
-    context '記録日の平均値 < 一つ前の記録日の平均値 の場合' do
-      before do
-        create(:record, user:, recorded_on:, weight: 59.0)
-      end
-
-      it ':smooth（＝順調）を返す' do
-        expect(trend).to eq :smooth
+    context '体重記録が２つ未満の場合' do
+      it 'nilを返す' do
+        expect(trend).to be_nil
       end
     end
   end
