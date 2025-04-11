2578760519d27ffc80a78f1ee396fcdefc111d35
sarii0213 <anjing.singka@gmail.com>
2025-04-11 14:33:45 +0900

    :bug: Userモデルのusernameのnull許可後のannotate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  username               :string(191)      default(""), not null
+#  username               :string(191)      default("")
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
