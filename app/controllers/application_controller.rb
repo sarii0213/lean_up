0f83a023c7383c845c3fa948b5cdbdfc949059b2
sarii0213 <anjing.singka@gmail.com>
2025-04-13 10:16:15 +0900

    :shirt: rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     login_path
   end
 end
