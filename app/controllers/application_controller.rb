9e6c866f170771f75b56e3c5b820432c1c93300e
sarii0213 <anjing.singka@gmail.com>
2025-04-13 10:08:38 +0900

    :fix: ログアウト後にルートパスではなくログイン画面に遷移するよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    login_path
+  end
 end
