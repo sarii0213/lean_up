c1143fa64b1252b680c26ad706e762df088b4184
sarii0213 <anjing.singka@gmail.com>
2025-05-03 16:08:05 +0900

    :green_heart: objective model specの修正
    subject, let, let!, beforeで読みやすく

--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -29,17 +29,18 @@ RSpec.describe Objective, type: :model do
   describe '#set_order' do
     context '初めてビジョンボードに登録する場合' do
       it '登録したobjectiveのorderが0になる' do
-        objective = build(:objective, user:)
-        objective.save
+        objective = create(:objective, user:)
         expect(objective.order).to eq(0)
       end
     end
 
     context 'ビジョンボードに追加する場合' do
-      it 'orderがobjectivesの数になる' do
+      before do
         4.times { create(:objective, user:) }
-        fifth_objective = create(:objective, user:)
+      end
 
+      it 'orderが作成済みobjectivesの数になる' do
+        fifth_objective = create(:objective, user:)
         expect(fifth_objective.order).to eq(4)
       end
     end
@@ -47,48 +48,62 @@ RSpec.describe Objective, type: :model do
 
   describe '#move_up!' do
     context '下に位置するobjectiveを上に移動させる場合' do
+      subject(:move_up) { objective.move_up! }
+
+      let!(:objective) { create(:objective, user:) }
+      let!(:upper_objective) { create(:objective, user:) }
+
       it 'objectiveのorderが1になる' do
-        objective = create(:objective, user:)
-        create(:objective, user:)
-        objective.move_up
-        expect(objective.reload.order).to eq(1)
+        expect { move_up }.to change { objective.reload.order }.from(0).to(1)
       end
 
       it '上のobjectiveのorderが0になる' do
-        objective = create(:objective, user:)
-        upper_objective = create(:objective, user:)
-        objective.move_up
-        expect(upper_objective.reload.order).to eq(0)
+        expect { move_up }.to change { upper_objective.reload.order }.from(1).to(0)
       end
     end
 
     context '一番上に位置するobjectiveを上に移動させる場合' do
-      it 'orderが最大のobjectiveの場合は入れ替わらない' do
+      subject(:move_up) { objective.move_up! }
+
+      before do
         4.times { create(:objective, user:) }
-        objective = create(:objective, user:)
-        objective.move_up
-        expect(objective.reload.order).to eq(4)
+      end
+
+      let(:objective) { create(:objective, user:) }
+
+      it 'orderが最大のobjectiveの場合は入れ替わらない' do
+        expect { move_up }.not_to change { objective.reload.order }.from(4)
       end
     end
   end
 
   describe '#move_down!' do
     context '上に位置するobjectiveを下に移動させる場合' do
-      it 'objectiveのorderがひとつ下のobjectiveのそれと入れ替わる' do
-        objective = create(:objective, user:)
-        lower_objective = create(:objective, user:)
-        objective.move_down
-        expect(objective.reload.order).to eq(0)
-        expect(lower_objective.reload.order).to eq(1)
+      subject(:move_down) { objective.move_down! }
+
+      let!(:lower_objective) { create(:objective, user:) }
+      let!(:objective) { create(:objective, user:) }
+
+      it 'objectiveのorderが0になる' do
+        expect { move_down }.to change { objective.reload.order }.from(1).to(0)
+      end
+
+      it '下のobjectiveのorderが1になる' do
+        expect { move_down }.to change { lower_objective.reload.order }.from(0).to(1)
       end
     end
 
     context '一番下に位置するobjectiveを下に移動させる場合' do
-      it 'orderが最小のobjectiveの場合は入れ替わらない' do
-        objective = create(:objective, user:)
+      subject(:move_down) { objective.move_down! }
+
+      let!(:objective) { create(:objective, user:) }
+
+      before do
         4.times { create(:objective, user:) }
-        objective.move_down
-        expect(objective.reload.order).to eq(0)
+      end
+
+      it 'orderが最小のobjectiveの場合は入れ替わらない' do
+        expect { move_down }.not_to change { objective.reload.order }.from(0)
       end
     end
   end
