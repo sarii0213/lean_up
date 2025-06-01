a33d865e7417b2a301b9e110f60eef0f4a4bb99b
sarii0213 <anjing.singka@gmail.com>
2025-06-01 17:07:39 +0900

    :sparkles: userモデルにomniauth strategy(line)適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:line]
+  
   # validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   validates :goal_weight, numericality: { greater_than: 0 }, allow_nil: true
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'strategies/line'
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -272,6 +274,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :line, ENV['LINE_CHANNEL_ID'], ENV['LINE_CHANNEL_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
