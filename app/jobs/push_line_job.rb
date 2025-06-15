82633eedc7b95019d3bc84b210f41af4a1f75a3e
sarii0213 <anjing.singka@gmail.com>
2025-06-15 19:53:34 +0900

    :sparkles: LINE配信のjobを作成

---
+++ app/jobs/push_line_job.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'line/bot'
+
+class PushLineJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    set_default_url_options
+
+    users = User.where.not(uid: nil).where(line_notify: true)
+    users.each do |user|
+      objective = user.objectives.sample
+      next if objective.blank?
+
+      message = build_message(objective)
+      request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(to: user.uid, messages: [message])
+      client.push_message(push_message_request: request)
+    end
+  end
+
+  private
+
+  def set_default_url_options
+    # dev環境でテスト時には、.envの値をngrokにて割り当てられたURLに都度変更する
+    host = ENV.fetch('LINE_BOT_HOST', nil)
+    Rails.application.routes.default_url_options[:host] = host
+    Rails.application.routes.default_url_options[:protocol] = 'https'
+  end
+
+  def build_message(objective)
+    case objective.objective_type
+    when 'image'
+      image = objective.images.sample
+      original_content_url = Rails.application.routes.url_helpers.url_for(image)
+      preview_image_url = Rails.application.routes.url_helpers.url_for(image.variant(:large))
+      Line::Bot::V2::MessagingApi::ImageMessage.new(original_content_url:, preview_image_url:)
+    when 'verbal'
+      text = objective.verbal
+      Line::Bot::V2::MessagingApi::TextMessage.new(text:)
+    end
+  end
+
+  def client
+    Line::Bot::V2::MessagingApi::ApiClient.new(
+      channel_access_token: ENV.fetch('LINE_BOT_CHANNEL_ACCESS_TOKEN', nil)
+    )
+  end
+end
