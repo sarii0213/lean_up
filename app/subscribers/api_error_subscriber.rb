0f78d1da1c6de0ff64221169716f64e3f68b09ac
sarii0213 <anjing.singka@gmail.com>
2025-06-23 16:18:33 +0900

    :sparkles: Slack通知を実装

--- app/subscribers/api_error_subscriber.rb
+++ app/subscribers/api_error_subscriber.rb
@@ -1,5 +1,43 @@
+# frozen_string_literal: true
+
 class ApiErrorSubscriber
-  def report(error, handled:, severity:, context:, source:)
-    Rails.logger.error("[rails_error] #{context}") # todo: slack通知にする
+  def report(_error, context:, **)
+    client = Slack::Web::Client.new
+    client.chat_postMessage(
+      channel: '#general',
+      text: 'LINE API request error',
+      blocks: build_message(context)
+    )
   end
+
+  private
+
+  # rubocop:disable Metrics/MethodLength
+  def build_message(context)
+    action = context[:action]
+    details = context.except(:action).map { |k, v| "*#{k}*: #{v}" }.join('\n ')
+
+    <<-"BLOCKS"
+    [
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": ":bell: #{action}"
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "#{details}"
+        }
+      }
+    ]
+    BLOCKS
+  end
+  # rubocop:enable Metrics/MethodLength
 end
---
+++ config/initializers/slack_ruby_client.rb
@@ -0,0 +1,3 @@
+Slack.configure do |config|
+  config.token = ENV['SLACK_LINE_BOT_TOKEN']
+end
