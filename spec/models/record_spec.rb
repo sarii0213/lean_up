2e8b6712ed539cff34f584751b795bccd34ff18f
sarii0213 <anjing.singka@gmail.com>
2025-08-07 19:21:28 +0300

    :white_check_mark: 生理時のメッセージ表示のspecを追加

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -55,4 +55,59 @@ RSpec.describe Record, type: :model do
       end
     end
   end
+
+  describe Record::MessageGenerator do
+    let(:recorded_on) { Date.new(2025, 1,1) }
+    let(:user) { create(:user) }
+    let(:generator) { described_class.new(recorded_on, user) }
+
+    describe '#generate' do
+      subject(:message) { generator.generate }
+
+      context '比較データがない場合' do
+        before do
+          allow(Record).to receive(:moving_average_trend).with(recorded_on).and_return(nil)
+        end
+
+        it 'nilを返す' do
+          expect(message).to be_nil
+        end
+      end
+
+      context '体重増加傾向で生理期間中の場合' do
+        let!(:period) { create(:period, user:, started_on: recorded_on - 1.day, ended_on: recorded_on + 5.days) }
+
+        before do
+          allow(Record).to receive(:moving_average_trend).with(recorded_on).and_return(:plateau)
+        end
+
+        it '生理中用のメッセージを返す' do
+          expected_messages = %w[プロゲステロンの影響で一時的に水分溜め込んでるだけ この時期に体重が増えるのは自然なこと ゆっくり休んで、ホルモンの乱れをやり過ごそう]
+          expect(expected_messages).to include(message)
+        end
+      end
+
+      context '体重増加傾向で生理期間外の場合' do
+        before do
+          allow(Record).to receive(:moving_average_trend).with(recorded_on).and_return(:plateau)
+        end
+
+        it '停滞期用のメッセージを返す' do
+          expected_messages = %w[現状把握できてて偉い!! 体重とかまじでただの数字 大きな増加や停滞はだいたい水分]
+          expect(expected_messages).to include(message)
+        end
+      end
+
+      context'体重減少傾向の場合' do
+        before do
+          allow(Record).to receive(:moving_average_trend).with(recorded_on).and_return(:smooth)
+        end
+
+        it '順調期用のメッセージを返す' do
+          expected_messages = %w[減っている!!すごい!! いい調子で減量中!!]
+          expect(expected_messages).to include(message)
+        end
+      end
+    end
+  end
 end
